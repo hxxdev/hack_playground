bb96f9511161a37144f0c761dfce798c6f377914
hxx.dev <hxx.dev@gmail.com>
2025-03-03 19:05:26 +0900

    env.sh bug fix

--- env.sh
+++ env.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-source "$PATH_DEV"/env.sh
 export PATH_VS="$PATH_WIN/Program Files/Microsoft Visual Studio/2022/Community/Common7/IDE/devenv.exe"
-export PATH_CE="$PATH_WIN/Cheat Engine 7.5"
+export PATH_CE="$PATH_WIN/Program Files/Cheat Engine 7.5/Cheat Engine.exe"
 export PATH_DBG="$PATH_WIN/Debugger"
-open_in_vs() {
+
+vs() {
    # Take first passed argument and generate absolute path
    FIRST_ARG=$(realpath $1)
    # Convert path (which is now in Unix style) to Windows path
@@ -12,12 +12,18 @@ open_in_vs() {
    # Run process using PowerShell
    powershell.exe "Start-Process '${PATH_TO_VS}' '${PATH_TO_FILE}'"
 }
-alias vs=open_in_vs
-alias ce="\"$PATH_CE\"/Cheat Engine.exe &"
+
+ce() {
+    local CE_PATH=$(wslpath -w "$PATH_CE")
+    powershell.exe "Start-Process '$CE_PATH'"
+}
+
 alias dbg32="\"$PATH_DBG\"/release/x32/x32dbg.exe &"
 alias dbg64="\"$PATH_DBG\"/release/x64/x64dbg.exe &"
+
 chmod u+x ./flare/:run_game
 chmod u+x ./flare/game/flare.exe
+
 echo "HACK ENV SETUP *******************************************"
 echo "VS                : $PATH_VS                              "
 echo "CHEAT ENGINE      : $PATH_CE								"
